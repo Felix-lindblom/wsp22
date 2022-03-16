3b7c99ca780edec4f6c9930b978380fa57f69f4a
Felix Lindblom <felix.lindblom@elev.ga.ntig.se>
2022-03-16 11:30:02 +0100

    0.2.0 liten start

---
+++ trading_kullavik/app.rb
@@ -0,0 +1,20 @@
+require "sinatra"
+require "slim"
+require "sqlite3"
+require "bcrypt"
+
+get ("/") do
+    slim (:register:)
+end
+
+post ("user/new") do
+    username = params[:username:]
+    password = params[:password:]
+    password_conf = params[:password_conf:]
+
+    if password == password_conf
+
+    else
+        "password är inte samma i fälten"
+    end
+end
