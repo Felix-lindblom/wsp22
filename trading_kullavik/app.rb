4d222ffd4e74c1bde8f0f263c5ef676d50c6a309
Felix Lindblom <felix.lindblom@elev.ga.ntig.se>
2022-05-18 10:49:12 +0200

    1.0.0 E uppnått

--- trading_kullavik/app.rb
+++ trading_kullavik/app.rb
@@ -3,6 +3,8 @@ require "slim"
 require "sqlite3"
 require "bcrypt"
 
+enable :sessions
+
 get ("/") do
     slim (:homepage)
 end
@@ -11,9 +13,35 @@ get ("/register") do
     slim (:register)
 end
 
+get ("/login") do
+    slim (:login)
+end
 
+post ("/inlog") do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new("db/centralhub.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE usertag = ?",username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+    if BCrypt::Password.new(pwdigest) == password
+        session[:id] = id
+        redirect('/scout')
+    else
+        "Fel lösenord"
+    end
+end
 
-post ("/user/new") do
+get ("/scout") do
+    if session[:id] != nil
+        slim(:scoutpage)
+    else
+        redirect('/register')
+    end
+end
+
+post ("/users/new") do
     username = params[:username]
     password = params[:password]
     password_conf = params[:password_conf]
